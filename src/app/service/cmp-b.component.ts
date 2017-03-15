cb876b66c740c3503c4ce98248aeb440e1be6197
scarol8 <caroline.stewart@modea.com>
2017-03-15 14:03:03 -0400

    first commit

---
+++ src/app/service/cmp-b.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LogService } from './log.service';
+
+
+@Component({
+    selector: 'si-cmp-b',
+    template: `
+    <div>
+      <input type="text" #input>
+      <button (click)="onLog(input.value)">Log</button>
+      <button (click)="onStore(input.value)">Store</button>
+    </div>
+    <hr>
+    <div>
+        <button (click)="onGet()">Refresh Storage</button>
+        <h3>Storage</h3>
+        <ul>
+            <li *ngFor="let item of items">{{item}}</li>
+        </ul>
+        <h3>Received Value</h3>
+        <p>{{value}}</p>
+    </div>
+  `,
+  providers: [LogService]
+
+})
+export class CmpBComponent implements OnInit {
+    value = '';
+    items: string[] = [];
+
+    constructor(private logService: LogService) {
+
+    }
+
+    onLog(value: string) {
+      this.logService.writeToLog(value);
+    }
+
+    onStore(value: string) {
+    }
+
+    onGet() {
+    }
+
+    ngOnInit() {
+    }
+}
---
+++ src/app/service/log.service.ts
@@ -0,0 +1,7 @@
+export class LogService {
+
+  writeToLog(logMessage: string) {
+    console.log(logMessage);
+  }
+
+}
